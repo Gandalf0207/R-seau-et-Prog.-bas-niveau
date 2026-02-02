bac5bd5ea307ccab114218ad8f554a4082b43b72
Kentus <quentin.pladeau@gmail.com>
2026-02-02 19:47:02 +0100

    Patch file's name

---
+++ FILES/TD2/files/ConstructMatriceQ10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Matrice matrice(int nbl, int nbc, int* valeur){
+    // création des tableau vide avec malloc
+    
+
+
+};
+
+
+int main() {
+
+};
--- FILES/TD2/files/matrice.c
+++ FILES/TD2/files/matrice.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Matrice matrice(int nbl, int nbc, int* valeur){
-    // création des tableau vide avec malloc
-    
+#include "matrice.h"
 
 
-};
-
+void affiche(struct Matrice mat) {
+    for(int i = 0; i < mat.nb_lignes; i++) {
+        for(int j = 0; j < mat.nb_colonnes; j++) {
+            printf("%3i", mat.valeurs[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
+    //TEST
 
-};
+    // int l1[3] = {1, 2, 3};
+    // int l2[3] = {4, 5, 6};
+    // int *v[2] = {l1, l2};
+
+    // struct Matrice m = {
+    //     .nb_lignes = 2,
+    //     .nb_colonnes = 3,
+    //     .valeurs = v,
+    // };
+    // affiche(m);
+}
