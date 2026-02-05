b38d0de2538e61273ba9340c06eb7df7f1175e3a
Kentus <quentin.pladeau@gmail.com>
2026-02-05 13:00:57 +0100

    test2

--- push.ps1
+++ push.ps1
@@ -6,8 +6,9 @@ echo "=== Synchronisation ==="
 echo "1. Push vers GitHub et GitLab backup..."
 git push origin main
 
-# Push du dossier TP3 vers gitlabFolderTP3
-echo "2. Push du dossier TD3 vers GitLab spécifique..."
-git subtree push --prefix=FILES/TD3 gitlabFolderTD3 master
+# Push du dossier TD3 vers GitLab spécifique (sur master avec force)
+Write-Host "2. Push du dossier TD3 vers GitLab spécifique..."
+$split = git subtree split --prefix=FILES/TD3 main
+git push gitlabFolderTD3 "${split}:refs/heads/master" # --force
 
 echo "=== Terminé ==="
